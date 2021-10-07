f53b19eb6b6d2957d082306d027d1c8a2f530b7a
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2021-10-07 17:30:45 +0700

    SLM

--- pySLM/SLM.py
+++ pySLM/SLM.py
@@ -2,29 +2,32 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import time
 import numpy as np
 import sounddevice as sd
 
-from PyQt5.QtCore import QThread, pyqtSignal
-from PyQt5.QtWidgets import QApplication, QDialog, QLineEdit, QLabel, QVBoxLayout
+from PyQt5.QtCore import Qt, QThread, pyqtSignal
+from PyQt5.QtWidgets import QApplication, QDialog, QLineEdit, QLabel
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QProgressBar
 
 class Audio(QThread):
     dbSPL = pyqtSignal(int)
-    volNormL = 0
-    volNormR = 0
-    gain = 1
+    volL = 0
+    volR = 0
+    ofsetL = 117
+    ofsetR = 115
 
     def getLevel(self, indata, frames, time, status):
-        self.volNormL = int(np.linalg.norm(indata[:,0]))*self.gain
-        self.volNormR = int(np.linalg.norm(indata[:,1]))*self.gain
-        volNorm = int(np.linalg.norm(indata))*self.gain
+        self.volL = round(20*np.log10(np.sqrt(np.mean(np.square(indata[:,0]))))+self.ofsetL,2)
+        self.volR = round(20*np.log10(np.sqrt(np.mean(np.square(indata[:,1]))))+self.ofsetR,2)
+        volNorm = int(np.linalg.norm(indata))
         self.dbSPL.emit(volNorm)
 
     def run(self):
         with sd.InputStream(callback=self.getLevel):
             print("Capture Started")
             while True:
-                pass
+                time.sleep(.001)
             print("Capture Stopped")
 
 class SLM(QDialog):
@@ -37,24 +40,61 @@ class SLM(QDialog):
 
     def initUI(self):
         self.setWindowTitle('Simple SLM')
+        self.setFixedSize(480,160)
 
         self.lblVolL = QLabel(self)
         self.lblVolL.setText('Volume L')
 
-        self.txtVolL = QLineEdit(self)
+        self.lblVolL0 = QLabel(self)
+        self.lblVolL0.setText('0 dB')
+        self.lblVolLU = QLabel(self)
+        self.lblVolLU.setText('140 dB')
 
+        self.barVolL = QProgressBar(self)
+        self.barVolL.setTextVisible(False)
+        self.barVolL.setFixedSize(250,25)
+        self.barVolL.setMaximum(140)
+
+        self.txtVolL = QLineEdit(self)
+        self.txtVolL.setReadOnly(True)
+        self.txtVolL.setFixedWidth(50)
+
+        self.vbVolL = QHBoxLayout()
+        self.vbVolL.addWidget(self.lblVolL)
+        self.vbVolL.addWidget(self.txtVolL)
+        self.vbVolL.addWidget(self.lblVolL0)
+        self.vbVolL.addWidget(self.barVolL)
+        self.vbVolL.addWidget(self.lblVolLU)
+        
         self.lblVolR = QLabel(self)
         self.lblVolR.setText('Volume R')
 
+        self.lblVolR0 = QLabel(self)
+        self.lblVolR0.setText('0 dB')
+        self.lblVolRU = QLabel(self)
+        self.lblVolRU.setText('140 dB')
+
+        self.barVolR = QProgressBar(self)
+        self.barVolR.setTextVisible(False)
+        self.barVolR.setFixedSize(250,25)
+        self.barVolR.setMaximum(140)
+
         self.txtVolR = QLineEdit(self)
+        self.txtVolR.setReadOnly(True)
+        self.txtVolR.setFixedWidth(50)
 
-        self.vbVol = QVBoxLayout()
-        self.vbVol.addWidget(self.lblVolL)
-        self.vbVol.addWidget(self.txtVolL)
-        self.vbVol.addWidget(self.lblVolR)
-        self.vbVol.addWidget(self.txtVolR)
+        self.vbVolR = QHBoxLayout()
+        self.vbVolR.addWidget(self.lblVolR)
+        self.vbVolR.addWidget(self.txtVolR)
+        self.vbVolR.addWidget(self.lblVolR0)
+        self.vbVolR.addWidget(self.barVolR)
+        self.vbVolR.addWidget(self.lblVolRU)
 
-        self.setLayout(self.vbVol)
+        self.hbVol = QVBoxLayout()
+        self.hbVol.addLayout(self.vbVolL)
+        self.hbVol.addLayout(self.vbVolR)
+
+        self.setLayout(self.hbVol)
 
         self.show()
 
@@ -68,8 +108,10 @@ class SLM(QDialog):
 
     def onSplchanged(self,val):
         splAvg = val
-        self.txtVolL.setText(str(self.spl.volNormL))
-        self.txtVolR.setText(str(self.spl.volNormR))
+        self.txtVolL.setText(str(self.spl.volL))
+        self.txtVolR.setText(str(self.spl.volR))
+        self.barVolL.setValue(int(self.spl.volL))
+        self.barVolR.setValue(int(self.spl.volR))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
